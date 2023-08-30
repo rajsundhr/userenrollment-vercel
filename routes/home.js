62f55be87dd6434cd6e877d681ac46a47a41d7d6
Muthuraj <54229054+Muthuraj-web@users.noreply.github.com>
2023-08-30 22:56:43 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/.well-known/com.apple.remotemanagement", async (req, res, next) => (req, res) => {
+router.get("/com.apple.remotemanagement", (req, res) => {
   // Simulating input stream data
   const responseText =
     '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://muthuraj-14527.csez.zohocorpin.com:443/mdm/client/v1/enroll?encapiKey=1G8rvZxvtMtAlFHdMt3uvB4o34pfOC%2BdzoBLEzN31VWG7%2B2IiDBLUsIkNKHL5AFHpXxj9KmV7iqZP%2FPnZ88bvJxvWL0rQxwGRFdCV48btXTsS6UEgv4m%2B9Y%3D&templateToken=e94c2f413ec46f783dc9d0fb9f3c4c0c"}]}';
