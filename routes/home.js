b685d52fd2fd046667648e1dd2ddb968193bb5ab
Muthuraj <54229054+Muthuraj-web@users.noreply.github.com>
2023-09-12 20:45:14 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/com.apple.remotemanagement", (req, res) => {
   // Simulating input stream data
   console.log("Sample")
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://dhanus-lin1.csez.zohocorpin.com:443/mdm/client/v1/enroll?encapiKey=1G8rvZw158JMkFSNMIrv6UJ4h4oKOynNndQYETsghg%2BKs%2BqIjDAbApdyZKvPsAZDqSxj8a3EvCqeO6fga5xNu884Ce8sFRwGRFdCV48btXTsFv4Eg%2FQmqg%3D%3D&templateToken=c60a5bd2958e12ab8fb634167ca1556d"}]}';
+  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm.manageengine.com.au/mdm/client/v1/enroll?encapiKey=GkDdjPjf%2BFYbzFyXp9noYtFUPshosJfqSs7kN80HpEN97dpbt0wcK8AblSUkmWpJsCaTDVaddO1y82SC47yDeigvdnyteUTuOpwzGB%2Bedd0F9TjSEfaw864%3D&templateToken=3cca80cace7130acbf01f623e0fa88f2"}]}';
   const inputStream = Buffer.from(responseText);
 
   const responseHeaders = {
