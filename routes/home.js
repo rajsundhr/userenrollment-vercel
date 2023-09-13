d8a10c45c8c4915f20dc16bd30479fa1dccd974a
Muthuraj <54229054+Muthuraj-web@users.noreply.github.com>
2023-09-13 16:16:45 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/com.apple.remotemanagement", (req, res) => {
   // Simulating input stream data
   console.log("Sample")
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm.manageengine.com.au/mdm/client/v1/enroll?encapiKey=GkDdjPjf%2BFYbzFyXp9noYtFUPshosJfqSs7kN80HpEN97dpbt0wcK8AblSUkmWpJsCaTDVaddO1y82SC47yDeigvdnyteUTuOpwzGB%2Bedd0F9TjSEfaw864%3D&templateToken=3cca80cace7130acbf01f623e0fa88f2"}]}';
+  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm.localzoho.com/mdm/client/v1/enroll?encapiKey=OlyO55TUNxYoxJUlfpaY2%2FOykhqt9KMKjXETa6W00mXDwHDgAx2jEcMCN3yXO5N0xa5N%2BvpZkaqoSZSLzHOQ45qlU7ql%2BY0KeItwz2yAguFPBGgVAJAIHWc%3D&templateToken=1c74c394477fbdfa8fe40dd7f3024ae0"}]}';
   const inputStream = Buffer.from(responseText);
 
   const responseHeaders = {
