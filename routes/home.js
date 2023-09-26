0eb1c341721cf72c536fdbceaf1cf550cd423d9c
muthuraj.dg@zohocorp.com <muthuraj.dg@zohocorp.com>
2023-09-26 14:15:04 +0530

    Addded training json

--- routes/home.js
+++ routes/home.js
@@ -1,57 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-// router.get("/com.apple.remotemanagement", (req, res) => {
-//   // Simulating input stream data
-//   console.log("Sample")
-//   const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm-win10-13.csez.zohocorpin.com:443/mdm/client/v1/enroll?encapiKey=1G8rvZxqtJ5MwQTRMdu8605yhYEPPyvNytoZRjgn1A%2BH7urb2TBKVM13N6HKtAcR%2Fio8pKzDunDMP%2FLjbJ4TuptqD7wrRRwGRFdCV48btXTsTKxY1aR5qtc%3D&templateToken=6821999e2b552a15ed458b4da9263c89"}]}';
-//   const inputStream = Buffer.from(responseText);
-
-//   const responseHeaders = {
-//     "Content-Type": "application/json",
-//     "Content-Length": inputStream.length
-//   };
-
-//   console.log(inputStream.length);
-//   res.writeHead(200, responseHeaders);
-
-//   // Process the input stream and send it as the response
-//   res.write(inputStream);
-//   res.end();
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-//-----------------------
-//-----------------------
-
-//BELOW LINES ARE JUST TO CHECK WITH 302 FLOW
-
-//-----------------------
-//-----------------------
-
-
-
-
-
-
-
-
-
-router.get("/com.apple.remotemanagement/ServiceDiscoveryData.json", (req, res) => {
+router.get("/com.apple.remotemanagement", (req, res) => {
   // Simulating input stream data
   console.log("Sample")
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm.manageengine.com.au/mdm/client/v1/enroll?encapiKey=GkDdjPjf%2BFYbzFyXp9noYtFUPshosJfqSs7kN80HpEN97dpbt0wcK8AblSUkmWpJsCaTDVaddO1y82SC47yDeigvdnyteUTuOpwzGB%2Bedd0F9TjSEfaw864%3D&templateToken=3cca80cace7130acbf01f623e0fa88f2"}]}';
+  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm-6514-u16.csez.zohocorpin.com:443/mdm/client/v1/enroll?encapiKey=1G8rvZxus88ZlFSLZo27u0ks1dcNOX3Ny9MWEGkgglSBvuSOjjAZBsdxM6GYsAIUri1io6%2FIuXDKO%2Fa1b5Qevs4zDL4gFBwGRFdCV48btXTsG%2F5e1vQq%2FIM%3D&templateToken=27fa3f50aaf00f64f05377dbc412f616"}]}'
   const inputStream = Buffer.from(responseText);
 
   const responseHeaders = {
@@ -67,23 +20,5 @@ router.get("/com.apple.remotemanagement/ServiceDiscoveryData.json", (req, res) =
   res.end();
 });
 
-
-router.get("/com.apple.remotemanagement", (req, res) => {
-  // Simulating input stream data
-  console.log("Sample")
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm.manageengine.com.au/mdm/client/v1/enroll?encapiKey=GkDdjPjf%2BFYbzFyXp9noYtFUPshosJfqSs7kN80HpEN97dpbt0wcK8AblSUkmWpJsCaTDVaddO1y82SC47yDeigvdnyteUTuOpwzGB%2Bedd0F9TjSEfaw864%3D&templateToken=3cca80cace7130acbf01f623e0fa88f2"}]}';
-  const inputStream = Buffer.from(responseText);
-
-  const responseHeaders = {
-    "Location":"https://www.userenrollment-vercel.vercel.app/.well-known/com.apple.remotemanagement/ServiceDiscoveryData.json"
-  };
-
-  console.log(inputStream.length);
-  res.writeHead(302, responseHeaders);
-
-  // Process the input stream and send it as the response
-  res.write(inputStream);
-  res.end();
-});
 
 module.exports = router;
