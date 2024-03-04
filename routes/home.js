acf63712553d9666651497c3423ee2c96f278788
Muthuraj <54229054+Muthuraj-web@users.noreply.github.com>
2024-03-04 16:35:11 +0530

    Bharath JSON

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/com.apple.remotemanagement", (req, res) => {
 
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm.manageengine.in/mdm/client/v1/enroll?encapiKey=PHtE6r0MFODoiG588kMB7aPpFpbxM9wm%2Bu00L1JPuYlBAqRQTk1S%2Ft4tkTTk%2BB4uXaRCEvaZydlhsbzKuu%2FWJz3vYW8dXWqyqK3sx%2FVYSPOZ86260EFLs1k%3D&templateToken=ec433e208bacdd058fcd2f1f372cf81e"}]}';
+  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://172.24.97.120:9383/mdm/client/v1/enroll?encapiKey=34970fe2&templateToken=501d912553d356491f45839a8e6b40c"}]}';
   const inputStream = Buffer.from(responseText);
 
   const responseHeaders = {
