037ec53aedad838ff64a8bcf713b6ae8d53ecde6
Muthuraj <54229054+Muthuraj-web@users.noreply.github.com>
2024-03-27 13:58:51 +0530

    Local File Changes

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/com.apple.remotemanagement", (req, res) => {
 
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://10.53.76.137:8443/mdm/client/v1/enroll?encapiKey=f02a6fa9&templateToken=cf91beb99e3160ceab3451abb11521c8"}]}';
+  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://10.53.76.137:8443/mdm/client/v1/enroll?encapiKey=1a2bfb7f&templateToken=dda167ac0ff9572015bfb9f036793e28"}]}';
   const inputStream = Buffer.from(responseText);
 
   const responseHeaders = {
